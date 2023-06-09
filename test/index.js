bc6b72103fd1f2952039cb5045db846b951750da
Carlos <carlos@prolydian.com>
2023-06-09 02:31:42 -0400

    added tests for internal and external monitors, improved existing tests

--- test/index.js
+++ test/index.js
@@ -1,50 +1,94 @@
 const lumi = require("../index.js");
 const {describe, it} = require("mocha");
-const {expect} = require("chai");
-const {sample, random} = require("lodash");
-const _ = require("lodash");
+const {expect, assert} = require("chai");
+const {sample, random, reduce} = require("lodash");
+const {randomUUID} = require("crypto");
 
 describe("lumi", function () {
     this.timeout(0);
+    
     it("should return available monitors", () => {
         const monitors = lumi.monitors();
         expect(monitors).to.not.be.empty;
     });
+    
     it("should return brightness", async function () {
         const monitors = lumi.monitors();
         const monitor = sample(monitors);
-        const {success, brightness} = await lumi.get(monitor.id);
-        if (success) return expect(brightness).to.be.a("number");
-        return expect(brightness).to.be.null;
+        const {brightness} = await lumi.get(monitor.id);
+        expect(brightness).to.be.a("number");
     });
-    it("should set global brightness", async function () {
-        const {success} = await lumi.set(lumi.GLOBAL, random(100));
-        return expect(success).to.be.true;
+
+    it("should return internal monitor brightness" , async () => {
+        const monitors = lumi.monitors();
+        const monitor = monitors.find(({internal}) => internal);
+        if (!monitor) return assert.fail("No internal monitors found");
+        const {brightness} = await lumi.get(monitor.id);
+        expect(brightness).to.be.a("number");
     });
+
+    it("should return external monitor brightness", async() => {
+        const monitors = lumi.monitors();
+        const monitor = monitors.find(({internal}) => !internal);
+        if (!monitor) return assert.fail("No external monitors found");
+        const {brightness} = await lumi.get(monitor.id);
+        expect(brightness).to.be.a("number");
+    });
+
     it("should set brightness", async function () {
         const monitors = lumi.monitors();
         const monitor = sample(monitors);
-        const {success, brightness} = await lumi.get(monitor.id);
-
-        if (brightness === null) return expect(success).to.be.false;
-
-        const {success: ableToSetBrightness} = await lumi.set(monitor.id, random(100));
-
-        if (brightness !== 0) await lumi.set(monitor.id, brightness);
-
-        return expect(ableToSetBrightness).to.be.true;
+        const {success} = await lumi.set(monitor.id, random(100));
+        expect(success).to.be.true;
     });
+
+    it("should set global brightness", async function () {
+        const {success} = await lumi.set(lumi.GLOBAL, random(100));
+        expect(success).to.be.true;
+    });
+    
+    it("should set internal monitor brightness", async function () {
+        const monitors = lumi.monitors();
+        const monitor = monitors.find(({internal}) => internal);
+
+        if (!monitor) return assert.fail("No internal monitors found");
+
+        const {success} = await lumi.set(monitor.id, random(100));
+
+        expect(success).to.be.true;
+    });
+
+    it("should set external monitor brightness", async () => {
+        const monitors = lumi.monitors();
+        const monitor = monitors.find(({internal}) => !internal);
+
+        if (!monitor) return assert.fail("No external monitors found");
+
+        const {success} = await lumi.set(monitor.id, random(100));
+
+        expect(success).to.be.true;
+    });
+    
     it("should set brightness with valid config", async () => {
         const monitors = lumi.monitors();
-        const config = _.reduce(monitors, (config, {id}) => ({
+        const config = reduce(monitors, (config, {id}) => ({
             ...config,
             [id]: random(100)
         }), {});
         const {success} = await lumi.set(config);
         expect(success).to.be.true;
     });
+
+    it("should fail to set brightness with invalid config", async () => {
+        const {success} = await lumi.set({
+            [randomUUID()]: random(100),
+            [randomUUID()]: random(100)
+        });
+        expect(success).to.be.false;
+    });
+    
     it("should fail to set brightness for non-existent monitor", async () => {
-        const {success} = await lumi.set("fake-monitor-id", 50);
+        const {success} = await lumi.set(randomUUID(), random(100));
         expect(success).to.be.false;
     });
 });
